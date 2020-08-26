f9bb9dfb3db347127d4ca55877c5efeafb6e4842
Michael <nivmichael@gmail.com>
2020-08-26 17:48:37 +0300

    Added Connection to mongo DB

---
+++ README.md
@@ -0,0 +1,2 @@
+Make sure to install new npm packages:  npm install  
+Make sure also that mongodb atlas user is created, and credential are changed in .env (users is temporary)
--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => { 
+    res.send('Hello MERN Tutorial')
+}) 
+
+try {
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+        useNewUrlParser: true,
+		useUnifiedTopology: true,
+    })
+    console.log('MongoDb connected successfully!')
+} catch (error) {
+    console.log(error)
+}
 
 app.listen(PORT, ()=>{
     console.log(`Listening on ${PORT}`)
