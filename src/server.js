138e7d0256887a20acdd634a6c6b0196234c03f4
Michael Niv <nivmichael@gmail.com>
2021-07-22 12:22:04 +0300

    test token

--- src/server.js
+++ src/server.js
@@ -6,26 +6,27 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+
 
 app.use(cors());
 app.use(express.json())
 
-try {  
+try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION, {
         useNewUrlParser: true,
-		useUnifiedTopology: true,
+        useUnifiedTopology: true,
     })
     console.log('MongoDb connected successfully!')
 } catch (error) {
     console.log(error)
-} 
+}
 
 app.use("/files", express.static(path.resolve(__dirname, "..", "files")))
 app.use(routes);
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on ${PORT}`) 
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
 })
